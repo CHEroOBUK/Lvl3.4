fade14efa950df0cbddc1f35b5bb06fcb8e87dc2
CHEroOBUK <Sapsan_90@mail.ru>
2020-08-01 22:30:37 +0300

    ДЗ 3.4

--- src/main/java/MFU.java
+++ src/main/java/MFU.java
@@ -1,5 +1,99 @@
 public class MFU {
-    public static void main(String[] args) {
+    private final Object printingBlocker = new Object();
+    private final Object scanningBlocker = new Object();
+    private volatile boolean isPrintLocked = false;
+    private volatile boolean isScanLocked = false;
 
+    public static void main(String[] args) {
+        final MFU mfu = new MFU();
+
+        Thread firstFloor = new Thread(() -> {
+            mfu.scan("Документ с 1 этажа");
+            mfu.copy("Документ с 1 этажа");
+            mfu.print("Документ с 1 этажа");
+        });
+        Thread secondFloor = new Thread(() -> {
+            mfu.copy("Документ с 2 этажа");
+            mfu.print("Документ с 2 этажа");
+            mfu.scan("Документ с 2 этажа");
+        });
+        Thread thirdFloor = new Thread(() -> {
+            mfu.print("Документ с 3 этажа");
+            mfu.scan("Документ с 3 этажа");
+            mfu.copy("Документ с 3 этажа");
+        });
+        firstFloor.start();
+        secondFloor.start();
+        thirdFloor.start();
+
+    }
+
+    public void print(String doc){
+        synchronized (printingBlocker) {
+            try {
+                while (isPrintLocked) {
+                    printingBlocker.wait();
+                }
+                isPrintLocked = true;
+                System.out.println("Выполнение печати " + doc + "...");
+                Thread.sleep(1000);
+                System.out.println(doc + " распечатан");
+                isPrintLocked = false;
+                printingBlocker.notifyAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void scan(String doc){
+        synchronized (scanningBlocker) {
+            try {
+                while (isScanLocked) {
+                    scanningBlocker.wait();
+                }
+                isScanLocked = true;
+                System.out.println("Выполнение сканирования " + doc + "...");
+                Thread.sleep(1000);
+                System.out.println(doc + " отсканирован");
+                isScanLocked = false;
+                scanningBlocker.notifyAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void copy(String doc){
+        synchronized (scanningBlocker){
+            try {
+                while (isScanLocked) {
+                    scanningBlocker.wait();
+                }
+                isScanLocked = true;
+                System.out.println("Выполнение ксерокопии " + doc + " (сканирование)...");
+                Thread.sleep(1000);
+                System.out.println(doc + " отсканирован");
+                isScanLocked = false;
+                scanningBlocker.notifyAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (printingBlocker){
+                try {
+                    while (isPrintLocked) {
+                        printingBlocker.wait();
+                    }
+                    isPrintLocked = true;
+                    System.out.println("Выполнение ксерокопии " + doc + " (печать)...");
+                    Thread.sleep(1000);
+                    System.out.println(doc + " распечатан");
+                    isPrintLocked = false;
+                    printingBlocker.notifyAll();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/main/java/ThreeThreads.java
+++ src/main/java/ThreeThreads.java
@@ -1,5 +1,69 @@
 public class ThreeThreads {
-    public static void main(String[] args) {
+    private final Object monitor = new Object();
+    private volatile char abc = 'C';
 
+    public static void main(String[] args) {
+        ThreeThreads threeThreads = new ThreeThreads();
+
+        Thread aMaker = new Thread(threeThreads::printA);
+        Thread bMaker = new Thread(threeThreads::printB);
+        Thread cMaker = new Thread(threeThreads::printC);
+
+        aMaker.start();
+        bMaker.start();
+        cMaker.start();
+        
     }
+
+    public void printA() {
+        synchronized (monitor) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (abc != 'C') {
+                        monitor.wait();
+                    }
+                    System.out.print("A");
+                    abc = 'A';
+                    monitor.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (monitor) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (abc != 'A') {
+                        monitor.wait();
+                    }
+                    System.out.print("B");
+                    abc = 'B';
+                    monitor.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (monitor) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (abc != 'B') {
+                        monitor.wait();
+                    }
+                    System.out.print("C");
+                    abc = 'C';
+                    monitor.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
